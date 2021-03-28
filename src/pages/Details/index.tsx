c4ee71eb6c64c77973236321f0e8dbda8a211183
obrenoco <58960887+obrenoco@users.noreply.github.com>
2021-03-28 18:52:22 -0300

    add pokemon api

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,12 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Link } from "brouther";
+import axios from "axios";
+
+interface DataProps {
+  name: string;
+  url: string;
+}
 
 const Details = () => {
+  const [pokemon, setPokemon] = useState<Array<DataProps>>([]);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        console.log(response);
+        setPokemon(response.data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getData();
+  }, []);
+
   return (
-    <Fragment>
+    <div>
       <h1>Details Page</h1>
-      <Link href="/">Return</Link>
-    </Fragment>
+      {pokemon.map((x) => (
+        <p key={x.name}>{x.name}</p>
+      ))}
+    </div>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,43 @@
-import { Fragment } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "brouther";
+import axios from "axios";
+
+interface DataProps {
+  name: string;
+  url: string;
+}
 
 const Home = () => {
+  const [pokemon, setPokemon] = useState<Array<DataProps>>([]);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        setPokemon(response.data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getData();
+  }, []);
+
+  function onClick(e: HTMLButtonElement) {
+    console.log(e.target.getAttribute("data-props"));
+  }
+
   return (
-    <Fragment>
+    <div>
       <h1>Home Page</h1>
-      <Link className="App-link" href="/details">
-        Detalhes
-      </Link>
-    </Fragment>
+      <ul>
+        {pokemon.map((x) => (
+          <li key={x.name} onClick={onClick as any}>
+            <Link href="#" data-props={x.url}>
+              {x.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
